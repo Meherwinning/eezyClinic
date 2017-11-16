4e44220c93e23623d1bb02eae85ce906c93eced2
chakri3130 <pabbathi_chakravarthy@tecnics.com>
2017-11-16 23:00:20 +0530

    Welcome Pagecontroller
    completed coding welcome pages without background image

---
+++ eezyclinic/eezyclinic/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  eezyclinic
+//
+//  Created by chakravarthy on 16/11/17.
+//  Copyright © 2017 chakravarthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
